bf704a7708ae60e5fc002c7aeb10d947814442ab
Narendra kumar <developerfeb2007@gmail.com>
2024-09-20 23:10:40 +0530

    dropdown-data modified

--- src/api/dropdown-data/controllers/dropdown-data.js
+++ src/api/dropdown-data/controllers/dropdown-data.js
@@ -12,35 +12,35 @@ module.exports = {
             select: ['id', 'value'],
             orderBy: { createdAt: 'ASC' },
         });
-        const mother_tongues = await strapi.db.query('api::gender.gender').findMany({
+        const mother_tongues = await strapi.db.query('api::language.language').findMany({
             select: ['id', 'value'],
             orderBy: { createdAt: 'ASC' },
         });
-        const proficiencies = await strapi.db.query('api::class.class').findMany({
+        const proficiencies = await strapi.db.query('api::fluency.fluency').findMany({
             select: ['id', 'value'],
             orderBy: { createdAt: 'ASC' },
         });
-        const school_boards = await strapi.db.query('api::gender.gender').findMany({
+        const school_boards = await strapi.db.query('api::school-board.school-board').findMany({
             select: ['id', 'value'],
             orderBy: { createdAt: 'ASC' },
         });
-        const completion_years = await strapi.db.query('api::class.class').findMany({
+        const completion_years = await strapi.db.query('api::completion-year.completion-year').findMany({
             select: ['id', 'value'],
             orderBy: { createdAt: 'ASC' },
         });
-        const profesions = await strapi.db.query('api::gender.gender').findMany({
+        const profesions = await strapi.db.query('api::profession.profession').findMany({
             select: ['id', 'value'],
             orderBy: { createdAt: 'ASC' },
         });
-        const relations = await strapi.db.query('api::class.class').findMany({
+        const relations = await strapi.db.query('api::relation.relation').findMany({
             select: ['id', 'value'],
             orderBy: { createdAt: 'ASC' },
         });
-        const annual_incomes = await strapi.db.query('api::gender.gender').findMany({
+        const annual_incomes = await strapi.db.query('api::annual-income.annual-income').findMany({
             select: ['id', 'value'],
             orderBy: { createdAt: 'ASC' },
         });
-        const sources = await strapi.db.query('api::class.class').findMany({
+        const sources = await strapi.db.query('api::source.source').findMany({
             select: ['id', 'value'],
             orderBy: { createdAt: 'ASC' },
         });
